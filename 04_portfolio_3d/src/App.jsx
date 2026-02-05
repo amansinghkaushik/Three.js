1a0147539163bf070a627c0f49c3b2b81452dbdd
Aman Kaushik <amansinghkaushik8@gmail.com>
2026-02-05 16:45:56 +0530

    Projects Section Done

--- 04_portfolio_3d/src/App.jsx
+++ 04_portfolio_3d/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import Navbar from "./Sections/Navbar"
 import Hero from "./Sections/Hero"
 import About from "./Sections/About"
+import Projects from "./Sections/Projects"
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Navbar />
         <Hero />
         <About />
+        <Projects />
       </main>
     </>
   )
