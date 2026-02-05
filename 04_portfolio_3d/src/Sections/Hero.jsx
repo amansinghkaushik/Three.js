cbd5facd4cbf4b7e81d0fb2170e1c2efabc323af
Aman Kaushik <amansinghkaushik8@gmail.com>
2026-02-05 14:33:24 +0530

    Hero Page Done

--- 04_portfolio_3d/src/Sections/Hero.jsx
+++ 04_portfolio_3d/src/Sections/Hero.jsx
@@ -11,6 +11,7 @@ import Cube from '../components/Cube.jsx'
 import Target from '../components/Target.jsx'
 import Rings from '../components/Rings.jsx'
 import HeroCamera from '../components/HeroCamera.jsx'
+import Button from '../components/Button.jsx'
 
 function Hero() {
 
@@ -96,6 +97,11 @@ function Hero() {
             <directionalLight position={[10, 10, 10]} intensity={0.5} />
             </Suspense>
         </Canvas>  
+      </div>
+      <div className="absolute bottom-7 left-0 right-0 w-full z-10 c-space">
+        <a href="#about" className="w-fit">
+          <Button name="Let's work together" isBeam containerClass="sm:w-fit w-full sm:min-w-96" />
+        </a>
       </div>
     </section>
   )
